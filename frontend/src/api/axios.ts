f2976f4facc4113f9a49b361fde3772c2c6a9692
Haicheng Zhao <2960799z@student.gla.ac.uk>
2025-08-10 14:10:51 +0100

    fix: Fixed production environment bug

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,11 +1,14 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 // Create an Axios instance with default config
 const httpClient: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:5173/api',
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://traind.online/api"
+      : "http://localhost:5173/api",
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
